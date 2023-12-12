ceed528c19cc73603f0165d651b591bd83eba713
GERSOME GARCERA <mcmeister17@gmail.com>
2023-12-12 16:48:08 +0800

    Added

--- Advanced Web Application/src/App.vue
+++ Advanced Web Application/src/App.vue
@@ -1,20 +1,12 @@
+<script setup>
+
+import BaseCondition from './components/BaseCondition.vue';
+import BaseLoop from './components/BaseLoop.vue';
+
+</script>
 <template>
-  <div id="app">
-    <input v-model="userInput" placeholder="Type something">
-    <p :style="{ color: textColor, fontSize: textSize + 'px' }">{{ userInput }}</p>
-    <label for="ChangeColor">Text Color:</label>
-    <input id="ChangeColor" type="color" v-model="textColor" />
-    <br>
-    <label for="fontSizeSlider">Font Size:</label>
-    <input id="fontSizeSlider" type="range" min="10" max="100" v-model="textSize" />
-    <p>Current Font Size: {{ textSize }}px</p>
-  </div>
+<BaseCondition />
+<BaseLoop />
 </template>
 
-<script setup>
-import { ref } from 'vue';
 
-const userInput = ref('');
-const textColor = ref('#000000');
-const textSize = ref(16);
-</script>
